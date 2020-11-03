ba04095ff29dceb2680c5f3fd004db43666fa1ae
jorgeagui <aguilera.go.jorge@gmail.com>
2020-11-03 14:44:02 +0100

    Inventory microservice mapper implemented.

---
+++ src/main/java/clothingstorefranchise/spring/inventory/mapper/ModelMapperConfig.java
@@ -0,0 +1,24 @@
+package clothingstorefranchise.spring.inventory.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import clothingstorefranchise.spring.inventory.dtos.StockDto;
+import clothingstorefranchise.spring.inventory.model.ShopStock;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	
+	@Bean
+    public ModelMapper modelMapper() {
+		var modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+	}
+	
+	
+}
